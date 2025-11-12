f0c62445fc27922b6f0097fec1dbf8baf6da0710
EliteBoB <elitebob@gmail.com>
2025-11-12 16:37:13 +1000

    update matrix payload length for paint.html

--- software/wordclock/src/src/Iot.cpp
+++ software/wordclock/src/src/Iot.cpp
@@ -904,7 +904,7 @@ void Iot::toggleDisplay_(String payload)
 }
 
 void Iot::setMatrixFromPayload_(String &payload) {
-  if (payload.length() >=132) {
+  if (payload.length() >=110) {
     display_->setMatrix(Palette::stringToRgb(payload, display_->getColor()));
   } else {
     DLOG("Matrix payload is too short :");
